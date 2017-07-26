8d72e09f1f7b42e93886350ebc5270424b1e831d
Sivaprakash Ragavan <mail@sivragav.com>
2017-07-26 17:40:12 -0400

    Initial Commit

---
+++ imports/ui/pages/Index/Index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+import './Index.scss';
+
+const Index = () => (
+  <div className="Index">
+    <img
+      src="/logo-500-black.png"
+      alt="Clovoice"
+    />
+    <h1>Clovoice</h1>
+    <p>Plug-n-Play Voice Stack for your application</p>
+    <div>
+      {/* <Button href="">Read the Docs</Button>
+      <Button href=""><i className="fa fa-star" /> Star on GitHub</Button> */}
+    </div>
+    <footer>
+      <p>APIs for Developers to capture, process & deliver voice content/features within their applications. <a href="">Read more</a>.
+      </p>
+    </footer>
+  </div>
+);
+
+export default Index;
---
+++ imports/ui/pages/Logout/Logout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Icon from '../../components/Icon/Icon';
+
+import './Logout.scss';
+
+const Logout = () => (
+  <div className="Logout">
+    <img
+      src="/logo-500-black.png"
+      alt="Clovoice"
+    />
+    <h1>Stay safe out there.</h1>
+    <p>{'Don\'t forget to like and follow Clovoice elsewhere on the web:'}</p>
+    <ul className="FollowUsElsewhere">
+      <li><a href="https://facebook.com/clovoice"><Icon icon="facebook-official" /></a></li>
+      <li><a href="https://twitter.com/clovoice"><Icon icon="twitter" /></a></li>
+      <li><a href="https://github.com/clovoice"><Icon icon="github" /></a></li>
+    </ul>
+  </div>
+);
+
+Logout.propTypes = {};
+
+export default Logout;
